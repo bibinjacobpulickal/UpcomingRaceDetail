44367e858bb16d9d277a12eb45fcfd880c26da7f
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2023-12-07 21:26:30 +1100

    Initial Commit

---
+++ Entain/EntainApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainApp.swift
+//  Entain
+//
+//  Created by Bibin Jacob Pulickal on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EntainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
